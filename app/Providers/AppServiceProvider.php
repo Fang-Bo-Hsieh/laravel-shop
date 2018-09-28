7d352d2020be0a87ffc7a23d5e505554faee288e
vagrant <vagrant@homestead>
2018-09-28 16:47:09 +0000

    add order pay feature

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,9 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
+            // 没有收到服务器端回调，这是因为我们的服务器运行在本地，https://shop.test 这个域名也只是对我们本地生效，支付宝服务器无法请求到我们的服务器端回调地址。 可以通过 requestbin 服务来帮我们捕获服务器端回调的数据
+            $config['notify_url'] = 'http://requestbin.fullcontact.com/1hj76jg1';
+            $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
                 $config['mode']         = 'dev';
