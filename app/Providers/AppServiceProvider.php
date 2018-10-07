b9e78221d7a3bdc276e23943b916ea5b57f98cb9
vagrant <vagrant@homestead>
2018-10-07 16:25:54 +0000

    add ngrok to dev env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,7 +37,8 @@ class AppServiceProvider extends ServiceProvider
             $config = config('pay.alipay');
             // 没有收到服务器端回调，这是因为我们的服务器运行在本地，https://shop.test 这个域名也只是对我们本地生效，支付宝服务器无法请求到我们的服务器端回调地址。 可以通过 requestbin 服务来帮我们捕获服务器端回调的数据
             // $config['notify_url'] = 'http://requestbin.fullcontact.com/1hj76jg1';
-            $config['notify_url'] = route('payment.alipay.notify');
+            // $config['notify_url'] = route('payment.alipay.notify');
+            $config['notify_url'] = ngrok_url('payment.alipay.notify');
             $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
@@ -52,7 +53,8 @@ class AppServiceProvider extends ServiceProvider
 
         $this->app->singleton('wechat_pay', function () {
             $config = config('pay.wechat');
-            $config['notify_url'] = route('payment.wechat.notify');
+            // $config['notify_url'] = route('payment.wechat.notify');
+            $config['notify_url'] = ngrok_url('payment.wechat.notify');
             if (app()->environment() !== 'production') {
                 $config['log']['level'] = Logger::DEBUG;
             } else {
